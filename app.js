3897cda4fecc15ae4c6ba744c125985521c4e3df
nowhereim <zyle1104@gmail.com>
2022-10-21 17:35:46 +0900

    'hello'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const router = require('./routes');
+const router = require("./routes");
 
 dotenv.config();
 const app = express();
@@ -8,7 +8,8 @@ app.use(express.json());
 
 app.use(router);
 
+console.log(123);
 
 app.listen(4000, () => {
-    console.log(4000, "포트로 서버가 열림");
-})
+  console.log(4000, "포트로 서버가 열림");
+});
