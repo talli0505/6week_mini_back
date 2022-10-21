1ce3bdd3da5cdbc9cbe1dcc744d51dd1c3cf0f99
talli0505@naver.com <talli0505@naver.com>
2022-10-21 15:56:59 +0900

    쏼라쏼라

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("2조")
+const express = require('express')
+const app = express()
+const port = 3000
+const router = require('./routes')
+
+app.use(express.json());
+app.use(router)
+
+app.listen(port, () => {
+  console.log(port, '포트로 서버가 열렸어요!')
+})
