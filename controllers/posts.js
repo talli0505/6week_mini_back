6de7dfc53b347dcee95f9645bb5d29b23430754f
talli0505@naver.com <talli0505@naver.com>
2022-10-26 01:19:15 +0900

    주석 처리 완료

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,18 +3,25 @@ const PostService = require("../services/posts");
 class PostsController {
   postService = new PostService();
 
-  // 전체 조회
+  // 게시물 전체 조회
   getPosts = async (req, res, next) => {
+
+    // postService 에 있는 findAllPost 함수를 받아와서 선언
     const posts = await this.postService.findAllPost();
 
     res.status(200).json({ data: posts });
   };
 
-  // 상세 조회
+  // 게시글 상세 조회
   getPostById = async (req, res, next) => {
     try {
+      // 경로 받아오기
       const { postId } = req.params;
+
+      // postService 에 있는 findPostById 함수를 받아와서 선언
       const post = await this.postService.findPostById(postId);
+
+      // 성공 : 선언한 값을 보내기, 실패 : 에러 메세지 보내기
       res.status(200).json({ post: post });
     } catch (err) {
       res.status(400).json({ err: err.message });
@@ -23,24 +30,37 @@ class PostsController {
 
   // 게시글 생성
   createPost = async (req, res, next) => {
+    // 입력 값 받아오기
     const { title, content } = req.body;
+
+    // 로그인 계정 (미들웨어) 에서 받아오기
     const { userId, nickname } = res.locals.user;
+
+    // postService 에 있는 createPost 함수를 받아와서 선언
     const createPostData = await this.postService.createPost(
       userId,
       nickname,
       title,
       content
     );
+
+    // 선언한 값을 보내기
     res.status(201).json({ data: createPostData });
   };
 
   // 게시글 수정
   updatePost = async (req, res, next) => {
     try {
+      // 경로 받아오기
       const { postId } = req.params;
+
+      // 입력 값 받아오기
       const { title, content } = req.body;
+
+      // 로그인 계정 (미들웨어) 에서 받아오기
       const { nickname } = res.locals.user;
 
+      // postService 에 있는 updatePost 함수를 받아와서 선언
       const updatePost = await this.postService.updatePost(
         postId,
         nickname,
@@ -48,6 +68,7 @@ class PostsController {
         content
       );
 
+      // 성공 : 선언한 값을 보내기, 실패 : 에러 메세지 보내기
       res.status(200).json({ data: updatePost });
     } catch (err) {
       res.status(400).json({ err: err.message });
@@ -57,11 +78,16 @@ class PostsController {
   // 게시글 삭제
   deletePost = async (req, res, next) => {
     try {
+      // 경로 받아오기
       const { postId } = req.params;
+
+      // 로그인 계정 (미들웨어) 에서 받아오기
       const { nickname } = res.locals.user;
 
+      // postService 에 있는 deletPost 함수를 받아와서 선언
       const deletePost = await this.postService.deletePost(postId, nickname);
 
+      // 성공 : 선언한 값을 보내기, 실패 : 에러 메세지 보내기
       res.status(200).json({ data: deletePost });
     } catch (err) {
       res.status(400).json({ err: err.message });
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,85 +1,13 @@
-const Joi = require("joi");
-const commentsrepository = require("../repositories/comments");
-const RE_COMMENT = /^[\s\S]{1,255}$/; // 댓글 정규 표현식
+const express = require("express");
+const router = express.Router();
 
-const commentSchema = Joi.object({
-  comment: Joi.string().pattern(RE_COMMENT).required(),
-});
+const UsersController = require("../controllers/users");
+const usersController = new UsersController();
 
-class Commentsservice {
-  commentrepository = new commentsrepository();
+// 회원가입
+router.post("/", usersController.createUserAccount);
 
-  createComment = async (postId, comment, userId) => {
-    try {
-      const resultSchema = commentSchema.validate({ comment: comment });
-      if (resultSchema.error) {
-        return "데이터 형식이 올바르지 않습니다.";
-      }
+ // 로그인
+router.post("/login", usersController.login);
 
-      const createcomment = await this.commentrepository.createComment(
-        postId,
-        comment,
-        userId
-      );
-      return createcomment;
-    } catch (error) {
-      return error.name + "=" + error.errorMessage;
-    }
-  };
-
-  Commentlist = async (postId) => {
-    try {
-      const comments = await this.commentrepository.Commentlist(postId);
-      if (comments.length === 0) {
-        return null;
-      }
-      return comments;
-    } catch (error) {
-      return error.name + "=" + error.errorMessage;
-    }
-  };
-
-  Commentedit = async (commentId, comment, userId) => {
-    try {
-      const resultSchema = commentSchema.validate({ comment: comment });
-      if (resultSchema.error) {
-        return "댓글 형식이 올바르지 않습니다.";
-      }
-      const Commentedit = await this.commentrepository.Commentedit(
-        commentId,
-        comment,
-        userId
-      );
-
-      if (Commentedit < 1) {
-        return "댓글이 존재하지 않습니다.";
-      }
-      return resultSchema;
-    } catch (error) {
-      return error.name + "=" + error.errorMessage;
-    }
-  };
-
-  Commentdelete = async (commentId, userId) => {
-    try {
-      const isExist = await this.commentrepository.CommentisExist(commentId);
-      if (!isExist) {
-        return "댓글이 없습니다.";
-      }
-
-      const Commentdelete = await this.commentrepository.Commentdelete(
-        commentId,
-        userId
-      );
-      if (Commentdelete < 1) {
-        return "본인만 삭제 가능합니다.";
-      }
-
-      return "댓글 삭제가 정상적으로 처리되었습니다.";
-    } catch (error) {
-      return error.name + "=" + error.errorMessage;
-    }
-  };
-}
-
-module.exports = Commentsservice;
+module.exports = router;
